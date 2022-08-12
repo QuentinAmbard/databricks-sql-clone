2abfea8f3fa71401bfc9f8d1ad10e8951fd69cee
arunpamulapati <arun.pamulapati@databricks.com>
2022-08-12 10:42:04 -0400

    change to take in the pat via command line to avoid PATs floating in files

--- import_dashboard_from_file.py
+++ import_dashboard_from_file.py
@@ -5,12 +5,12 @@ from utils.client import Client
 import json
 
 
-def get_client(config_file):
+def get_client(config_filei,pat_token):
     with open(config_file, "r") as r:
         config = json.loads(r.read())
         targets = []
         for target in config["targets"]:
-            client = Client(target["url"], target["token"], permissions=target["permissions"])
+            client = Client(target["url"],pat_token, permissions=target["permissions"])
             if "endpoint_id" in target:
                 client.endpoint_id = target["endpoint_id"]
             if "sql_database_name" in target:
@@ -21,12 +21,12 @@ def get_client(config_file):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config_file", default="config_import.json", required=False,
-                    help="configuration file containing credential and dashboard to clone")
-parser.add_argument("--state_file", default="state.json", required=False,
-                    help="state containing the links between the already cloned dashboard. Used to update resources")
+                    help="Configuration file containing credential and dashboard to clone")
+parser.add_argument("--pat_token", required=Truee,
+                    help="Personal Access Token to for your workspace")
 args = parser.parse_args()
 
-target_clients, dashboard_id_to_load,dashboard_folder  = get_client(args.config_file)
+target_clients, dashboard_id_to_load,dashboard_folder  = get_client(args.config_file, args.pat_token)
 workspace_state = {}
 load_dashboard(target_clients[0], dashboard_id_to_load, workspace_state, dashboard_folder)
                                                          
